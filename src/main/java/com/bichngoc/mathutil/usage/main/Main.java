86f01f5c629ef967bb1ee17e97b14230ff4183f4
sapphire-pham <ngocbichpham2110@gmail.com>
2023-10-12 11:41:38 +0700

    1st upload

---
+++ src/main/java/com/bichngoc/mathutil/usage/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bichngoc.mathutil.usage.main;
+
+import com.bichngoc.mathutil.core.MathUtility;
+
+/**
+ *
+ * @author ngocb
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Using my own library - mathutility");
+        System.out.println("6! = 720: " + MathUtility.getFactorial(6));
+    }
+}
